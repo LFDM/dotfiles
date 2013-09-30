74bdcd09a49be5da809ad4ff3be79e11e02c25eb
LFDM <1986gh@gmail.com>
2013-09-30 15:51:07 +0200

    Adds git-nuke functions.

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -3,3 +3,11 @@ function git-on-master {
   git checkout '$(current_branch)'
   git rebase master
 }
+
+function git-nuke {
+  git branch -d $1 && git push origin :$1
+}
+
+function git-nuke! {
+  git branch -D $1 && git push origin :$1
+}
