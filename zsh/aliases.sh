673e0a08cf5032002113619f8bda793a46c3af5a
LFDM <1986gh@gmail.com>
2013-12-04 14:49:11 +0100

    Changes llt aliases to llt-dev

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -7,13 +7,17 @@ alias dots='cd ~/dotfiles'
 
 # Active projects
 alias llt=' cd ~/code/llt/'
-alias lltc='cd ~/code/llt/llt-core'
-alias lltl='cd ~/code/llt/llt-logger/'
-alias llth='cd ~/code/llt/llt-helpers'
-alias lltd='cd ~/code/llt/llt-db_handler'
-alias lltk='cd ~/code/llt/llt-constants'
-alias llte='cd ~/code/llt/llt-core_extensions'
-alias lltf='cd ~/code/llt/llt-form_builder'
+alias lltdev='cd ~/code/llt-dev/'
+alias lltm='cd ~/code/llt-dev/llt'
+alias lltc='cd ~/code/llt-dev/llt-core'
+alias lltl='cd ~/code/llt-dev/llt-logger/'
+alias llth='cd ~/code/llt-dev/llt-helpers'
+alias lltd='cd ~/code/llt-dev/llt-db_handler'
+alias lltk='cd ~/code/llt-dev/llt-constants'
+alias llts='cd ~/code/llt-dev/llt-segmenter'
+alias llte='cd ~/code/llt-dev/llt-core_extensions'
+alias lltf='cd ~/code/llt-dev/llt-form_builder'
+alias lltt='cd ~/code/llt-dev/llt-tokenizer'
 alias mlld='cd ~/code/multi_language_latin_dictionary'
 alias diploma='cd ~/code/diploma_thesis'
 
@@ -39,11 +43,19 @@ alias v='vim'
 alias vimrc='vim ~/.vimrc.after'
 alias zshrc='vim ~/.zshrc'
 alias pryrc='vim ~/.pryrc'
+alias tmuxc='vim ~/.tmux.conf'
 alias rsnips='vim ~/dotfiles/snippets/ruby.snippets'
 alias csnips='vim ~/dotfiles/snippets/clojure.snippets'
 alias msnips='vim ~/dotfiles/snippets/markdown.snippets'
 alias aliases='vim ~/.zsh/aliases.sh'
 alias zsh_func='vim ~/.zsh/functions.sh'
+
+# Start the dual screen tmux sessions
+alias tmux_start='~/.scripts/tmux_dual_screen_setup.sh'
+
+# Connect with tmux sessions
+alias tmux_main='tmux -2 attach-session -d -t main'
+alias tmux_aux='tmux -2 attach-session -d -t aux'
 
 # Faster opening
 alias open='xdg-open'
