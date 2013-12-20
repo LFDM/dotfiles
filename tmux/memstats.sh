63e21b700c95178d8e8078f4a7767b43663b69eb
LFDM <1986gh@gmail.com>
2013-12-20 22:45:20 +0100

    more accurate memstats

--- tmux/memstats.sh
+++ tmux/memstats.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 total=$(($(egrep 'MemTotal' /proc/meminfo | cut -d " " -f 9) / 1024))
-free=$(($(egrep 'MemFree' /proc/meminfo | cut -d " " -f 10) / 1024))
-used=$((total - free))
+used=$(($(egrep 'Active:' /proc/meminfo | cut -d " " -f 11) / 1024))
 
 echo "$((used/ (total / 100)))% of ${total}mb"
