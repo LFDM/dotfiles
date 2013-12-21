e2378171ef2bf5c08981d87f4b7b77eb7174ebb3
LFDM <1986gh@gmail.com>
2013-12-21 01:14:57 +0100

    Minor.

--- tmux/home_session.sh
+++ tmux/home_session.sh
@@ -5,13 +5,10 @@ tmux new-window -t home:2 -n diploma
 tmux new-window -t home:8 -n rake-runner
 tmux new-window -t home:9 -n cmus
 
-tmux send-keys -t home:1 'c' C-m
-tmux send-keys -t home:2 'diploma' C-m
-tmux send-keys -t home:9 'cmus' C-m
-
 # Hacky solution to get a clean screen on startup
-tmux send-keys -t home:1 'clear' C-m
-tmux send-keys -t home:2 'clear' C-m
+tmux send-keys -t home:1 'c; clear' C-m
+tmux send-keys -t home:2 'diploma; clear' C-m
+tmux send-keys -t home:9 'cmus' C-m
 
 # No cmus updates
 tmux set-window-option -t home:9 monitor-activity off
