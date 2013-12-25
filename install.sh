da1a52b56d62dd682a2f60c9f6ded0d7b97ceb5a
LFDM <1986gh@gmail.com>
2013-12-25 12:36:59 +0100

    Starting the installation script.

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-inst () { sudo apt-get install $1 ; }
-log () { echo "\033[93m\n######### $1 #########\033[0m" ; }
-loginst () { log "Installing $1" && inst $1 ; }
+inst () { apt-get install $1 ; }
+with_color() { echo "\033[$2m$1\033[0m" ; }
+suc () { with_color "\n### $1 ###" 92 ;}
+log () { with_color "\n######### $1 #########\033[0m" 93 ; }
+hint () { with_color "Hint: $1", 36 ; }
+loginst () { log "Installing $1" ; }
+log_and_inst () { loginst $1 && inst $1 ; }
+dotfiles_dir=$PWD
 
-log 'Beginning Installation!'
+suc 'Beginning Installation!'
 
-loginst 'curl'
+log_and_inst 'curl'
 
 log 'Installing rvm'
 #\curl -sSL https://get.rvm.io | bash
@@ -17,5 +22,29 @@ log 'Getting MRI ruby'
 log 'Getting rake'
 #gem install rake
 
-# and do the rest via a rake task
-rake install
+#loginst 'zsh'
+log 'Making zsh the default shell'
+chsh -s /bin/zsh
+
+log_and_inst 'tmux'
+
+log_and_inst 'vim-athena'
+
+loginst 'janus'
+#curl -Lo- https://bit.ly/janus-bootstrap | bash
+
+log_and_inst 'exuberant-ctags'
+
+loginst 'ack'
+hint "It's called ack-grep on ubuntu platforms"
+inst 'ack-grep'
+ln -sf /usr/bin/ack-grep /usr/local/bin/ack
+hint "Linked ack-grep to ack"
+
+loginst 'leiningen'
+curl -s https://raw.github.com/technomancy/leiningen/stable/bin/lein > /bin/lein
+chmod a+x /bin/lein
+hint 'Run lein to finish the installation process'
+
+suc "Installations complete! Starting rake"
+rake
