632de9b1db76d23f1c139032954330ae70c28d7e
LFDM <1986gh@gmail.com>
2013-12-25 16:48:49 +0100

    Refactor install script.

--- install.sh
+++ install.sh
@@ -1,32 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-inst () { apt-get install $1 ; }
-with_color() { echo "\033[$2m$1\033[0m" ; }
+inst () { sudo apt-get install $1 ; }
+with_color() { echo -e "\033[$2m$1\033[0m" ; }
 suc () { with_color "\n### $1 ###" 92 ;}
 log () { with_color "\n######### $1 #########\033[0m" 93 ; }
-hint () { with_color "Hint: $1", 36 ; }
+hint () { with_color "Hint: $1" 36 ; }
 loginst () { log "Installing $1" ; }
 log_and_inst () { loginst $1 && inst $1 ; }
+gem_inst () {
+  rvm use jruby ;
+  gem install "$@" ;
+  rvm use ruby ;
+  gem install "$@" ;
+}
 dotfiles_dir=$PWD
 
-suc 'Beginning Installation!'
+suc 'Beginning Installation'
 
 log_and_inst 'curl'
 
 log 'Installing rvm'
-#\curl -sSL https://get.rvm.io | bash
+#curl -SL https://get.rvm.io | bash
+
+# need to source rvm in this script separately to run it
+source "$HOME/.rvm/scripts/rvm"
+
+#log_and_inst 'zsh'
+sudo chsh -s /bin/zsh
+hint 'zsh set as default shell'
+
+log_and_inst 'tmux'
 
 log 'Getting MRI ruby'
 #rvm install ruby
 
-log 'Getting rake'
-#gem install rake
-
-#loginst 'zsh'
-log 'Making zsh the default shell'
-chsh -s /bin/zsh
-
-log_and_inst 'tmux'
+log 'Getting jruby'
+#rvm install jruby
+# not sure if we have to switch or if it would be done automatically
+log 'Getting gems rake andbundler'
+gem_inst rake bundler
 
 log_and_inst 'vim-athena'
 
@@ -42,9 +54,14 @@ ln -sf /usr/bin/ack-grep /usr/local/bin/ack
 hint "Linked ack-grep to ack"
 
 loginst 'leiningen'
-curl -s https://raw.github.com/technomancy/leiningen/stable/bin/lein > /bin/lein
+curl https://raw.github.com/technomancy/leiningen/stable/bin/lein > /bin/lein
 chmod a+x /bin/lein
-hint 'Run lein to finish the installation process'
+hint 'The installation will complete itself the first time you run lein'
 
-suc "Installations complete! Starting rake"
+log 'Creating ~/code directory'
+mkdir ~/code
+
 rake
+
+suc 'Success!'
+echo "Now close this terminal and start a new one. Enjoy!"
