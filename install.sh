e01e5b740fecf6dd1c1062b8b60b033f1def1a16
LFDM <1986gh@gmail.com>
2013-12-25 11:20:42 +0100

    Deleting empty lines.

--- install.sh
+++ install.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 inst () { sudo apt-get install $1 ; }
-
 log () { echo "\033[93m\n######### $1 #########\033[0m" ; }
-
 loginst () { log "Installing $1" && inst $1 ; }
 
 log 'Beginning Installation!'
@@ -21,5 +19,3 @@ log 'Getting rake'
 
 # and do the rest via a rake task
 rake install
-
-
