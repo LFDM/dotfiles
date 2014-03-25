5bcbb7d78ad28e437ad6c43d39da969cacce2f85
LFDM <1986gh@gmail.com>
2014-03-25 09:37:28 +0100

    Update tmux home session

--- tmux/home_session.sh
+++ tmux/home_session.sh
@@ -3,13 +3,11 @@
 tmux has-session -t home 2>/dev/null
 if [ "$?" -eq 1 ]; then
   tmux new -d -s home -n code
-  tmux new-window -t home:2 -n diploma
   tmux new-window -t home:8 -n rake-runner
   tmux new-window -t home:9 -n cmus
 
   # Hacky solution to get a clean screen on startup
   tmux send-keys -t home:1 'c; clear' C-m
-  tmux send-keys -t home:2 'diploma; clear' C-m
   tmux send-keys -t home:9 'cmus' C-m
 
   # No cmus updates
